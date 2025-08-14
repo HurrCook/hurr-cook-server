b1feb527e83bb5e4f481f8f628ef43a4393fd2d0
kimmoongi <moongi0320@naver.com>
2025-08-14 16:19:12 +0900

    [HC-9]: feat: JwtUtil 내부에 createToken 메서드 생성

---
+++ src/main/java/com/github/hurrcook/global/property/JwtProperty.java
@@ -0,0 +1,18 @@
+package com.github.hurrcook.global.property;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "app.jwt")
+public class JwtProperty {
+
+    @NotBlank
+    private String secretKey;
+
+    @NotBlank
+    Long expiration;
+}
--- src/main/java/com/github/hurrcook/global/security/jwt/JwtUtil.java
+++ src/main/java/com/github/hurrcook/global/security/jwt/JwtUtil.java
@@ -1,10 +1,39 @@
 package com.github.hurrcook.global.security.jwt;
 
+import com.auth0.jwt.JWT;
+import com.github.hurrcook.domain.user.entity.User;
+import com.github.hurrcook.global.property.JwtProperty;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.auth0.jwt.algorithms.Algorithm;
 
-@Component
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Configuration
+@RequiredArgsConstructor
 public class JwtUtil {
+
+    private final JwtProperty jwtProperty;
+
+    @Bean
+    public Algorithm algorithm(){
+
+        return Algorithm.HMAC256(jwtProperty.getSecretKey());
+    }
+
+    public String createToken(User user){
+
+        return JWT.create()
+                .withIssuedAt(Instant.now())
+                .withExpiresAt(Instant.now().plus(jwtProperty.getExpiration(), ChronoUnit.HOURS))
+                .withClaim("id",user.getId().toString())
+                .sign(algorithm());
+    }
+
 
     public String extractToken(HttpServletRequest request){
 
