e6635e516d0b4c6081d69d347c99234d6cae3f99
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-14 17:34:43 +0900

    [HC-11] feat: CascadeType.ALL 및 orphanRemoval=true 적용

--- src/main/java/com/github/hurrcook/domain/user/entity/User.java
+++ src/main/java/com/github/hurrcook/domain/user/entity/User.java
@@ -2,10 +2,7 @@ package com.github.hurrcook.domain.user.entity;
 
 import com.github.hurrcook.domain.cookware.entity.Cookware;
 import com.github.hurrcook.global.infra.BaseSchema;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -19,6 +16,6 @@ public class User extends BaseSchema {
     @Column(nullable = false)
     String name;
 
-    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
+    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private Cookware cookware;
 }
