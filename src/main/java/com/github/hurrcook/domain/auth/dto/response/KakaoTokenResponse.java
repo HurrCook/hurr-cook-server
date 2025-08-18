63d4fb1631fa8af3f70a963ce14b79de266318b7
docherryra <docherryra@gmail.com>
2025-08-19 03:53:34 +0900

    [HC-7] feat(auth): kakao token 응답에 scope 필드 추가

--- src/main/java/com/github/hurrcook/domain/auth/dto/response/KakaoTokenResponse.java
+++ src/main/java/com/github/hurrcook/domain/auth/dto/response/KakaoTokenResponse.java
@@ -9,15 +9,16 @@ public record KakaoTokenResponse(
         @JsonProperty("access_token") String accessToken,
         @JsonProperty("expires_in") Integer expiresIn, // 액세스 토큰 ttl, 단위 seconds
         @JsonProperty("refresh_token") String refreshToken,
-        @JsonProperty("refresh_token_expires_in") Integer refreshTokenExpiresIn
+        @JsonProperty("refresh_token_expires_in") Integer refreshTokenExpiresIn,
+        String scope
 ) {
-    public static KakaoTokenResponse from(String tokenType, String accessToken, Integer expiresIn, String refreshToken, Integer refreshTokenExpiresIn){
+    public static KakaoTokenResponse from(String tokenType, String accessToken, Integer expiresIn, String refreshToken, Integer refreshTokenExpiresIn,  String scope) {
         return KakaoTokenResponse.builder()
                 .tokenType(tokenType)
                 .accessToken(accessToken)
                 .expiresIn(expiresIn)
                 .refreshToken(refreshToken)
                 .refreshTokenExpiresIn(refreshTokenExpiresIn)
-                .build();
-    }
+                .scope(scope)
+                .build();}
 }
