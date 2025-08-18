5ef06a409d3e118d1a21a51eaf625ffdb2dbb47d
docherryra <docherryra@gmail.com>
2025-08-19 04:35:00 +0900

    [HC-7] feat(auth): 사용자 정보 조회 응답 구조 변경, id 필드 추가

--- src/main/java/com/github/hurrcook/domain/auth/dto/response/KakaoUserInfoResponse.java
+++ src/main/java/com/github/hurrcook/domain/auth/dto/response/KakaoUserInfoResponse.java
@@ -1,7 +1,18 @@
 package com.github.hurrcook.domain.auth.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record KakaoUserInfoResponse (
-        String nickname
-)
-{
+        Long id,
+        @JsonProperty("kakao_account") KakaoAccount kakaoAccount
+
+) {
+    public record KakaoAccount(
+            Profile profile
+
+    ) {
+        public record Profile(
+                String nickname
+        ){}
+    }
 }
