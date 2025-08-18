f27fca14ba8b7c191da036f63b9be91f514dd017
docherryra <docherryra@gmail.com>
2025-08-19 01:52:49 +0900

    [HC-7] feat(auth): 로그인 페이지 리다이렉트 api 구현

--- src/main/java/com/github/hurrcook/domain/auth/controller/AuthController.java
+++ src/main/java/com/github/hurrcook/domain/auth/controller/AuthController.java
@@ -1,4 +1,24 @@
 package com.github.hurrcook.domain.auth.controller;
 
-public class authController {
+import com.github.hurrcook.domain.auth.service.AuthService;
+import com.github.hurrcook.global.response.ApiResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    // client에서 로그인 요청 -> 로그인 페이지로 리다이렉트
+    @GetMapping("/kakao/login")
+    public void redirectToKakaoLogin(HttpServletResponse response) throws IOException {
+        response.sendRedirect(authService.getKakaoLoginUrl());
+//        return ApiResponse.ok();
+    }
+
 }
--- src/main/java/com/github/hurrcook/domain/auth/service/AuthService.java
+++ src/main/java/com/github/hurrcook/domain/auth/service/AuthService.java
@@ -1,4 +1,28 @@
 package com.github.hurrcook.domain.auth.service;
 
-public class authService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    @Value("${oauth.kakao.client-id}")
+    private String clientId;
+
+    @Value("${oauth.kakao.redirect-uri}")
+    private String redirectUri;
+
+    private final String KAKAO_AUTH_URL = "https://kauth.kakao.com/oauth/authorize";
+    private final String KAKAO_TOKEN_URL = "https://kauth.kakao.com/oauth/token";
+
+    public String getKakaoLoginUrl(){ // 로그인 페이지로의 리다이렉트 Url 반환
+        return UriComponentsBuilder.fromHttpUrl(KAKAO_AUTH_URL)
+                .queryParam("client_id", clientId)
+                .queryParam("redirect_uri", redirectUri)
+                .queryParam("response_type", "code")
+                .toUriString();
+    }
 }
