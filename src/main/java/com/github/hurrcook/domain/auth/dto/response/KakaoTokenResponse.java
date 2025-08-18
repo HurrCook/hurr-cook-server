4df3485e4341d953c0ab70e905fbe1a476bbc9ca
docherryra <docherryra@gmail.com>
2025-08-19 03:37:24 +0900

    [HC-7] feat(auth): kakao token reponse dto 및 token dto 추가

--- src/main/java/com/github/hurrcook/domain/auth/dto/response/KakaoTokenResponse.java
+++ src/main/java/com/github/hurrcook/domain/auth/dto/response/KakaoTokenResponse.java
@@ -4,15 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 @Builder
-public record KakaoTokenResponseDto (
+public record KakaoTokenResponse(
         @JsonProperty("token_type") String tokenType,
         @JsonProperty("access_token") String accessToken,
         @JsonProperty("expires_in") Integer expiresIn, // 액세스 토큰 ttl, 단위 seconds
         @JsonProperty("refresh_token") String refreshToken,
         @JsonProperty("refresh_token_expires_in") Integer refreshTokenExpiresIn
 ) {
-    public static KakaoTokenResponseDto from(String tokenType, String accessToken, Integer expiresIn, String refreshToken, Integer refreshTokenExpiresIn){
-        return KakaoTokenResponseDto.builder()
+    public static KakaoTokenResponse from(String tokenType, String accessToken, Integer expiresIn, String refreshToken, Integer refreshTokenExpiresIn){
+        return KakaoTokenResponse.builder()
                 .tokenType(tokenType)
                 .accessToken(accessToken)
                 .expiresIn(expiresIn)
--- src/main/java/com/github/hurrcook/domain/auth/dto/response/TokenResponse.java
+++ src/main/java/com/github/hurrcook/domain/auth/dto/response/TokenResponse.java
@@ -1,4 +1,17 @@
 package com.github.hurrcook.domain.auth.dto.response;
 
-public record TokenResponseDto () {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+
+@Builder
+public record TokenResponse(
+        @JsonProperty("access_token") String accessToken,
+        @JsonProperty("refresh_token") String refreshToken
+) {
+    public static TokenResponse from(String accessToken, String refreshToken) {
+        return TokenResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
 }
