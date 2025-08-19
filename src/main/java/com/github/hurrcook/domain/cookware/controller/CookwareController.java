8a0c9b00c913163e977aaf9a79513397d640b1e5
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-19 12:57:26 +0900

    [HC-13] feat(cookware): 요리도구 조회 컨트롤러 기능

---
+++ src/main/java/com/github/hurrcook/domain/cookware/controller/CookwareController.java
@@ -0,0 +1,26 @@
+package com.github.hurrcook.domain.cookware.controller;
+
+import com.github.hurrcook.domain.cookware.dto.response.CookwareResponse;
+import com.github.hurrcook.domain.cookware.service.CookwareService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class CookwareController {
+
+    private final CookwareService cookwareService;
+
+    @GetMapping("/cookwares")
+    public ResponseEntity<CookwareResponse> getCookware(@AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        UUID userId = userDetails.getUserId();
+        CookwareResponse cookwareResponse = cookwareService.getCookwareByUserId(userId);
+        return ResponseEntity.ok(cookwareResponse);
+    }
+
+}
