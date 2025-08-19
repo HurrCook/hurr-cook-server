9457cd2f6da253bd6272017148c911d49d92272d
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-19 13:34:36 +0900

    [HC-13] feat(cookware): 요리도구 repository findByUserId 추가

--- src/main/java/com/github/hurrcook/domain/cookware/repository/CookwareRepository.java
+++ src/main/java/com/github/hurrcook/domain/cookware/repository/CookwareRepository.java
@@ -3,7 +3,10 @@ package com.github.hurrcook.domain.cookware.repository;
 import com.github.hurrcook.domain.cookware.entity.Cookware;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CookwareRepository extends JpaRepository<Cookware, UUID> {
+
+    Optional<Cookware> findByUserId(UUID userId);
 }
