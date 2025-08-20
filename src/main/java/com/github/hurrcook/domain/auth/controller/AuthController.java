43d7dd19d08c86789cc1529997eba510b0beffb3
docherryra <docherryra@gmail.com>
2025-08-20 21:31:07 +0900

    [HC-7] refactor(config): yml에 공통 api 엔드포인트 지정

--- src/main/java/com/github/hurrcook/domain/auth/controller/AuthController.java
+++ src/main/java/com/github/hurrcook/domain/auth/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 @Validated
 public class AuthController {
