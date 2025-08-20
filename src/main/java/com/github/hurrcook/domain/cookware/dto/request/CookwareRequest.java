59961ee94112c3a9680feeffcc850c88af9baa06
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-20 14:05:42 +0900

    [HC-13] refactor(cookware): 요리도구 request dto Notnull 어노테이션 추가

--- src/main/java/com/github/hurrcook/domain/cookware/dto/request/CookwareRequest.java
+++ src/main/java/com/github/hurrcook/domain/cookware/dto/request/CookwareRequest.java
@@ -1,23 +1,32 @@
 package com.github.hurrcook.domain.cookware.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CookwareRequest {
 
+    @NotNull
     private boolean hasPot;
 
+    @NotNull
     private boolean hasPan;
 
+    @NotNull
     private boolean hasCooker;
 
+    @NotNull
     private boolean hasSteamer;
 
+    @NotNull
     private boolean hasOven;
 
+    @NotNull
     private boolean hasMicro;
 
+    @NotNull
     private boolean hasToaster;
 
+    @NotNull
     private boolean hasAirFryer;
 }
