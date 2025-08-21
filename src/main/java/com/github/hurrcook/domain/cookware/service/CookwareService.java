50c66ac86482c6f967de4caddba0fb7fd7bf0087
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-21 12:48:55 +0900

    [HC-13] refactor(cookware): user의 요리도구가 없을 때 디폴트값 할당 후 반환

--- src/main/java/com/github/hurrcook/domain/cookware/service/CookwareService.java
+++ src/main/java/com/github/hurrcook/domain/cookware/service/CookwareService.java
@@ -3,10 +3,8 @@ package com.github.hurrcook.domain.cookware.service;
 import com.github.hurrcook.domain.cookware.dto.request.CookwareRequest;
 import com.github.hurrcook.domain.cookware.dto.response.CookwareResponse;
 import com.github.hurrcook.domain.cookware.entity.Cookware;
-import com.github.hurrcook.domain.cookware.exception.CookwareException;
 import com.github.hurrcook.domain.cookware.repository.CookwareRepository;
 import com.github.hurrcook.domain.user.entity.User;
-import com.github.hurrcook.global.exception.ApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,7 +19,9 @@ public class CookwareService {
     public CookwareResponse getCookwareByUser(User user) {
 
         Cookware cookware = cookwareRepository.findByUser(user)
-                .orElseThrow(() -> new ApiException(CookwareException.COOKWARE_NOT_FOUND));
+                .orElse(Cookware.builder()
+                        .user(user)
+                        .build());
 
         return CookwareResponse.from(cookware);
     }
