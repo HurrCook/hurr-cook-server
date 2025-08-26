98eeae0611030a1ab1e615d802762403ee039820
docherryra <docherryra@gmail.com>
2025-08-27 03:12:49 +0900

    [HC-37] feat(auth): Refresh token 엔티티 추가

---
+++ src/main/java/com/github/hurrcook/domain/user/repository/RefreshTokenRedisRepository.java
@@ -0,0 +1,14 @@
+package com.github.hurrcook.domain.user.repository;
+
+
+import com.github.hurrcook.domain.auth.entity.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RefreshTokenRedisRepository extends CrudRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
