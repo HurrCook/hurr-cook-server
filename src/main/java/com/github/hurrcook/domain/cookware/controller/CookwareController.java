22fab96b95fc67b09fd66cace78eb93aa7461284
kimmoongi <moongi0320@naver.com>
2025-08-26 14:16:03 +0900

    [HC-7] refactor(cookware): 의존성 추가

--- src/main/java/com/github/hurrcook/domain/cookware/controller/CookwareController.java
+++ src/main/java/com/github/hurrcook/domain/cookware/controller/CookwareController.java
@@ -4,9 +4,9 @@ import com.github.hurrcook.domain.cookware.dto.request.CookwareRequest;
 import com.github.hurrcook.domain.cookware.dto.response.CookwareResponse;
 import com.github.hurrcook.domain.cookware.service.CookwareService;
 import com.github.hurrcook.domain.user.entity.User;
+import com.github.hurrcook.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
