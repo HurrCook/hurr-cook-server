0e4cd293eaeffc812ec669d8d967808e18f60a1c
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-27 14:05:48 +0900

    [HC-21] feat(swagger): swagger 설정 파일 생성

---
+++ src/main/java/com/github/hurrcook/global/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.github.hurrcook.global.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI().components(new Components().addSecuritySchemes("JWT", securityScheme())).info(info());
+    }
+
+    private Info info() {
+
+        return new Info()
+                .title("HurrCook")
+                .description("자취생 냉장고 레시피 추천 어플")
+                .version("0.2.0");
+    }
+
+    private SecurityScheme securityScheme() {
+
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP).bearerFormat("JWT").scheme("bearer");
+    }
+}
