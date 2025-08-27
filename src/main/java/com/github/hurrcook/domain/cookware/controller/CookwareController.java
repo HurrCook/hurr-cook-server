45eee2bb3b7a18493aa60abf1669bd845cc3124c
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-27 14:10:18 +0900

    [HC-21] refactor(swagger): 요리도구 컨트롤러 Tag, Operation 어노테이션 추가

--- src/main/java/com/github/hurrcook/domain/cookware/controller/CookwareController.java
+++ src/main/java/com/github/hurrcook/domain/cookware/controller/CookwareController.java
@@ -4,6 +4,9 @@ import com.github.hurrcook.domain.cookware.dto.request.CookwareRequest;
 import com.github.hurrcook.domain.cookware.dto.response.CookwareResponse;
 import com.github.hurrcook.domain.cookware.service.CookwareService;
 import com.github.hurrcook.domain.user.entity.User;
+import com.github.hurrcook.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +15,13 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "요리도구")
 public class CookwareController {
 
     private final CookwareService cookwareService;
 
     @GetMapping("/cookwares")
+    @Operation(summary = "요리도구 조회")
     public ApiResponse<CookwareResponse> getCookware(@AuthenticationPrincipal User user) {
 
         CookwareResponse cookwareResponse = cookwareService.getCookwareByUser(user);
@@ -24,6 +29,7 @@ public class CookwareController {
     }
 
     @PostMapping("/cookwares")
+    @Operation(summary = "요리도구 등록")
     public ApiResponse<Void> saveCookware(@AuthenticationPrincipal User user, @RequestBody @Valid CookwareRequest cookwareRequest) {
 
         cookwareService.saveCookware(user, cookwareRequest);
