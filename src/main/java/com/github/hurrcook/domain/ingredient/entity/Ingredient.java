07800ebaff08fb1288818059c1740d40c4634d27
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-27 14:27:42 +0900

    [HC-33] faet(swagger): 재료 엔티티 생성

---
+++ src/main/java/com/github/hurrcook/domain/ingredient/entity/Ingredient.java
@@ -0,0 +1,35 @@
+package com.github.hurrcook.domain.ingredient.entity;
+
+import com.github.hurrcook.domain.user.entity.User;
+import com.github.hurrcook.global.common.Unit;
+import com.github.hurrcook.global.infra.BaseSchema;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Ingredient extends BaseSchema {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false)
+    int amount;
+
+    @Column(nullable = false)
+    LocalDateTime expire_time;
+
+    @Column(nullable = false)
+    Unit unit;
+}
