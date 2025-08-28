45192d36ba92ff9b22c53b8067bd1d38e6eae818
kimmoongi <moongi0320@naver.com>
2025-08-28 23:50:50 +0900

    [HC-23] refactor(unit): unit 저장 시 소문자 입력시 대문자로 변경 로직 구현

--- src/main/java/com/github/hurrcook/global/common/Unit.java
+++ src/main/java/com/github/hurrcook/global/common/Unit.java
@@ -1,7 +1,14 @@
 package com.github.hurrcook.global.common;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Unit {
-    ml,
-    g,
-    EA
+    ML,
+    G,
+    EA;
+
+    @JsonCreator
+    public static Unit fromString(String value) {
+        return Unit.valueOf(value.toUpperCase());
+    }
 }
