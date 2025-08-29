780f9421a46a8bb3c4cf984e7b18c017eeaf8121
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-29 17:35:14 +0900

    [HC-27] feat(ingredient): 엔티티에 from 메서드 추가

---
+++ src/main/java/com/github/hurrcook/domain/ingredient/entity/Ingredient.java
@@ -0,0 +1,46 @@
+package com.github.hurrcook.domain.ingredient.entity;
+
+import com.github.hurrcook.domain.ingredient.dto.request.IngredientRequest;
+import com.github.hurrcook.domain.user.entity.User;
+import com.github.hurrcook.global.common.Unit;
+import com.github.hurrcook.global.infra.BaseSchema;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Ingredient extends BaseSchema {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false)
+    int amount;
+
+    @Column(nullable = false)
+    LocalDateTime expire_time;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    Unit unit;
+
+    public static Ingredient from(User user, IngredientRequest request) {
+        return Ingredient.builder()
+                .user(user)
+                .name(request.getName())
+                .amount(request.getAmount())
+                .expire_time(request.getExpire_time())
+                .unit(request.getUnit())
+                .build();
+    }
+}
