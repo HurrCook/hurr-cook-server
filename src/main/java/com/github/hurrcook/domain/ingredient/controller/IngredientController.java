78e0c64c945c73f59b2cb3fcab8127da879e3576
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-29 17:34:46 +0900

    [HC-27] feat(ingredient): 재료 등록 컨트롤러 생성

---
+++ src/main/java/com/github/hurrcook/domain/ingredient/controller/IngredientController.java
@@ -0,0 +1,29 @@
+package com.github.hurrcook.domain.ingredient.controller;
+
+import com.github.hurrcook.domain.ingredient.dto.request.IngredientRequest;
+import com.github.hurrcook.domain.ingredient.service.IngredientService;
+import com.github.hurrcook.domain.user.entity.User;
+import com.github.hurrcook.global.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/ingredients")
+public class IngredientController {
+
+    private final IngredientService ingredientService;
+
+    @PostMapping("/add")
+    public ApiResponse<Void> addIngredient(@AuthenticationPrincipal User user, @RequestBody @Valid IngredientRequest ingredientRequest) {
+
+        ingredientService.addIngredient(user, ingredientRequest);
+
+        return ApiResponse.ok();
+    }
+}
