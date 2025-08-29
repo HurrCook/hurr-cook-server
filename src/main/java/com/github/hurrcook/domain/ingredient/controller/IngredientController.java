535b8b31108d55ff773cf3487aec9e529cc330e2
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-08-30 01:13:14 +0900

    [HC-27] refactor(ingredient): 컨트롤러, dto swagger 적용

--- src/main/java/com/github/hurrcook/domain/ingredient/controller/IngredientController.java
+++ src/main/java/com/github/hurrcook/domain/ingredient/controller/IngredientController.java
@@ -4,6 +4,8 @@ import com.github.hurrcook.domain.ingredient.dto.request.IngredientRequest;
 import com.github.hurrcook.domain.ingredient.service.IngredientService;
 import com.github.hurrcook.domain.user.entity.User;
 import com.github.hurrcook.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,11 +19,13 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/ingredients")
+@Tag(name = "재료")
 public class IngredientController {
 
     private final IngredientService ingredientService;
 
     @PostMapping("/add")
+    @Operation(summary = "재료 등록")
     public ApiResponse<Void> addIngredient(@AuthenticationPrincipal User user, @RequestBody @Valid List<IngredientRequest> ingredientRequests) {
 
         ingredientService.addIngredient(user, ingredientRequests);
