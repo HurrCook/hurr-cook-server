0105c175e8af27330fc1f847c8774f6c4d5315a3
docherryra <docherryra@gmail.com>
2025-09-01 23:24:00 +0900

    [HC-29] feat(ingredient): 재료 목록 조회 서비스 구현

--- src/main/java/com/github/hurrcook/domain/ingredient/service/IngredientService.java
+++ src/main/java/com/github/hurrcook/domain/ingredient/service/IngredientService.java
@@ -2,6 +2,7 @@ package com.github.hurrcook.domain.ingredient.service;
 
 import com.github.hurrcook.domain.ingredient.IngredientRepository;
 import com.github.hurrcook.domain.ingredient.dto.request.IngredientRequest;
+import com.github.hurrcook.domain.ingredient.dto.response.IngredientResponse;
 import com.github.hurrcook.domain.ingredient.entity.Ingredient;
 import com.github.hurrcook.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
@@ -28,5 +29,14 @@ public class IngredientService {
         }
 
         ingredientRepository.saveAll(ingredients);
+    }
+
+    // 유저가 가진 모든 재료 조회
+    @Transactional(readOnly = true)
+    public List<IngredientResponse> getIngredients(User user) {
+        return ingredientRepository.findIngredientByUser(user)
+                .stream()
+                .map(IngredientResponse::from)
+                .toList();
     }
 }
