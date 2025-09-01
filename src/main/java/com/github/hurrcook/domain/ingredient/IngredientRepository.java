ff29d0dd4be2caccc093c5d179934717d78f06c0
IyeonHan <104899885+docherryra@users.noreply.github.com>
2025-09-02 00:33:56 +0900

    [0.2.0] HC-29 재료 목룍 조회 API (#26)
    * [HC-29] feat(ingredient): 엔티티 필드명 카멜케이스로 변경
    
    * [HC-29] feat(ingredient): 재료 목록 조회 dto 추가
    
    * [HC-29] feat(ingredient): 재료 목록 조회 컨트롤러 추가
    
    * [HC-29] feat(ingredient): 재료 목록 조회 서비스 구현
    
    * [HC-29] feat(ingredient): 레포지토리 메서드명 변경
    
    * [HC-29] feat(ingredient): 레포지토리 메서드명 반영

--- src/main/java/com/github/hurrcook/domain/ingredient/IngredientRepository.java
+++ src/main/java/com/github/hurrcook/domain/ingredient/IngredientRepository.java
@@ -1,9 +1,12 @@
 package com.github.hurrcook.domain.ingredient;
 
 import com.github.hurrcook.domain.ingredient.entity.Ingredient;
+import com.github.hurrcook.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, UUID> {
+    List<Ingredient> findAllByUser(User user);
 }
