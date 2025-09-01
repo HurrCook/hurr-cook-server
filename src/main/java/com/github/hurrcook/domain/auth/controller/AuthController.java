cf69e25506ddfd9f8908290b51f83772c9f65d6d
docherryra <docherryra@gmail.com>
2025-09-01 22:31:19 +0900

    [HC-35] feat(auth): 로그아웃 컨트롤러 추가

--- src/main/java/com/github/hurrcook/domain/auth/controller/AuthController.java
+++ src/main/java/com/github/hurrcook/domain/auth/controller/AuthController.java
@@ -3,8 +3,10 @@ package com.github.hurrcook.domain.auth.controller;
 import com.github.hurrcook.domain.auth.dto.response.LoginResponse;
 import com.github.hurrcook.domain.auth.service.AuthService;
 import com.github.hurrcook.global.response.ApiResponse;
+import com.github.hurrcook.global.security.jwt.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
@@ -20,18 +22,25 @@ import java.io.IOException;
 @Tag(name = "인증")
 public class AuthController {
     private final AuthService authService;
+    private final JwtUtil jwtUtil;
 
     // client에서 로그인 요청 -> 로그인 페이지로 리다이렉트
     @GetMapping("/kakao/login")
     @Operation(summary = "로그인 요청")
     public void redirectToKakaoLogin(HttpServletResponse response) throws IOException {
         response.sendRedirect(authService.getKakaoLoginUrl());
-//        return ApiResponse.ok();
     }
 
     @GetMapping("/kakao/callback") // 사용자 인증 후 쿼리 파라미터에 인가 코드를 담아 callback 되어 토큰 발급 요청
     @Operation(summary = "토큰 발급 요청")
     public ApiResponse<LoginResponse> callback(@RequestParam("code") @NotBlank String authorizeCode) {
         return ApiResponse.ok(authService.kakaoLogin(authorizeCode));
+    }
+
+    @GetMapping("/logout")
+    @Operation(summary = "로그아웃", description = "사용자 세션 종료: 리프레시 토큰 삭제, 액세스 토큰 블랙리스트 처리")
+    public ApiResponse<Void> logout(HttpServletRequest request) { // 현재 액세스 토큰으로 요청
+        authService.logout(jwtUtil.extractToken(request));
+        return ApiResponse.ok();
     }
 }
