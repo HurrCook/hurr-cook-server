9e20ca89dc4205061a5de0be608f95224ad70291
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-09-02 13:57:02 +0900

    [HC-28] feat(ingredient): 재료 차감 리포지토리 메서드 추가

--- src/main/java/com/github/hurrcook/domain/ingredient/IngredientRepository.java
+++ src/main/java/com/github/hurrcook/domain/ingredient/IngredientRepository.java
@@ -1,9 +1,14 @@
 package com.github.hurrcook.domain.ingredient;
 
 import com.github.hurrcook.domain.ingredient.entity.Ingredient;
+import com.github.hurrcook.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, UUID> {
+
+    List<Ingredient> findByUserAndIdIn(User user, Collection<UUID> ingredientIds);
 }
