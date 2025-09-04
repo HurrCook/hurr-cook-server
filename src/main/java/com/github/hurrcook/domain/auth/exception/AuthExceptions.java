4c680382d05fe58bb0a141380f5049e36d296d20
kimmoongi <moongi0320@naver.com>
2025-09-04 19:26:17 +0900

    [HC-38] feat(JWT): 토큰에 type 추가

--- src/main/java/com/github/hurrcook/domain/auth/exception/AuthExceptions.java
+++ src/main/java/com/github/hurrcook/domain/auth/exception/AuthExceptions.java
@@ -13,7 +13,9 @@ public enum AuthExceptions implements ApiExceptions {
     AUTHENTICATION_FAILED("인증에 실패했습니다."),
     KAKAO_TOKEN_ISSUE_FAILED("카카오 토큰 발급에 실패했습니다."),
     KAKAO_USERINFO_REQUEST_FAILED("카카오 사용자 정보 조회에 실패했습니다."),
-    INVALID_USER_REQUEST("잘못된 사용자 요청입니다.");
+    INVALID_USER_REQUEST("잘못된 사용자 요청입니다."),
+    REFRESH_TOKEN_NOT_ALLOWED("리프레시 토큰은 사용할 수 없습니다");
+
 
 
     private final String message;
