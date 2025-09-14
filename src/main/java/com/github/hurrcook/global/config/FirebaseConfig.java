e34bacc644021d316bfae36a7abedcbc4d49c3a7
kimmoongi <moongi0320@naver.com>
2025-09-15 00:03:49 +0900

    [HC-41] config(firebase):fcm 기능을 위한 firebase 초기화 설정 추가

---
+++ src/main/java/com/github/hurrcook/global/config/FirebaseConfig.java
@@ -0,0 +1,29 @@
+package com.github.hurrcook.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void firestore() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/hurrcook-firebase-adminsdk-fbsvc-1d3a454c52.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+    }
+
+
+}
