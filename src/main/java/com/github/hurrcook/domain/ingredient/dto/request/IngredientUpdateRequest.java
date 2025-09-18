ec1f24c75004b52ee25afc01d6ffef166bd33ad2
SeWoong110 <timothy11081@kookmin.ac.kr>
2025-09-18 15:30:58 +0900

    [HC-42] feat(ingredient): 재료 정보 수정 dto 생성

---
+++ src/main/java/com/github/hurrcook/domain/ingredient/dto/request/IngredientUpdateRequest.java
@@ -0,0 +1,25 @@
+package com.github.hurrcook.domain.ingredient.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class IngredientUpdateRequest {
+
+    @NotBlank
+    @Schema(description = "재료 이름")
+    String name;
+
+    @Positive
+    @Schema(description = "수량")
+    int amount;
+
+    @NotNull
+    @Schema(description = "유통기한")
+    LocalDateTime expireDate;
+}
