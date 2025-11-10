a5393daedbb41e35363a16f7bed75493afa8bcaf
kimmoongi <moongi0320@naver.com>
2025-11-10 23:28:08 +0900

    feat: rename OcrRequest to ImageRequest for improved clarity

--- src/main/java/com/github/hurrcook/domain/chat/dto/request/ImageRequest.java
+++ src/main/java/com/github/hurrcook/domain/chat/dto/request/ImageRequest.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
-public class OcrRequest {
+public class ImageRequest {
     @Schema(description = "ocr 인식 이미지 인코딩 문자열")
     private String base64_image;
 }
